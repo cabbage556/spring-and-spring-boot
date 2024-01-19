b741a07fd2b8f941b85709251021e7a46ac78ad3
cabbage556 <liberty556@gmail.com>
2024-01-19 15:27:36 +0900

    add: LoginUser 어노테이션, spring-session-jdbc 의존성

--- src/main/java/com/cabbage556/book/springboot/web/IndexController.java
+++ src/main/java/com/cabbage556/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.cabbage556.book.springboot.web;
 
+import com.cabbage556.book.springboot.config.auth.LoginUser;
 import com.cabbage556.book.springboot.config.auth.dto.SessionUser;
 import com.cabbage556.book.springboot.service.posts.PostsService;
 import com.cabbage556.book.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
