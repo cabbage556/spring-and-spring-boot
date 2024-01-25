a22cab2fbb21cab6bec63480d059a6d7910206f3
cabbage556 <liberty556@gmail.com>
2024-01-25 22:42:44 +0900

    LazyInitializationLauncher.java 패키지 이동

--- src/main/java/com/liberty556/learnspringframework/lazyEagerInit/LazyInitializationLauncher.java
+++ src/main/java/com/liberty556/learnspringframework/lazyEagerInit/LazyInitializationLauncher.java
@@ -1,12 +1,10 @@
-package com.liberty556.learnspringframework.lazyEagerInit.ex01;
+package com.liberty556.learnspringframework.lazyEagerInit;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
 
 @Component
 class ClassA {}
