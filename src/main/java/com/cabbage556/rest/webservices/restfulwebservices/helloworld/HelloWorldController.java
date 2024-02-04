e43ad09a60f5c4418aceca8573ec19ac93a6e4f5
cabbage556 <liberty556@gmail.com>
2024-02-04 20:18:31 +0900

    Content Negotiation, 국제화-i18n

--- src/main/java/com/cabbage556/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/cabbage556/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.cabbage556.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController  // REST API 컨트롤러
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     // @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     @GetMapping(path = "/hello-world")
@@ -25,5 +35,16 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    // i18n 메서드
+    @GetMapping(path = "/hello-world-i18n")
+    public String helloWorldi18n() {
+        // LocaleContextHolder.getLocale() 유틸리티 메서드로 로케일 가져오기
+        //      Accept-Language 헤더의 값에 대한 로케일을 가져옴
+        Locale locale = LocaleContextHolder.getLocale();
+        System.out.println(locale.getDisplayLanguage());
+        return messageSource
+                .getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
