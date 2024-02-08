942d80dbfb1842f54475ba0b45eb1ea6fc0d1c76
cabbage556 <liberty556@gmail.com>
2024-02-08 17:39:52 +0900

    Initial commit

---
+++ src/test/java/com/cabbage556/mockito/mockitodemo/MockitoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabbage556.mockito.mockitodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
